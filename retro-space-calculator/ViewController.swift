1731518e27f0882c54e4f0977722a0ca4c971fde
Kevin Gilboy <kevingilboy@m-j2051-01-ml.local>
2015-11-06 10:58:38 -0600

    Initial Commit

---
+++ retro-space-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retro-space-calculator
+//
+//  Created by Kevin Gilboy on 11/6/15.
+//  Copyright © 2015 Kevin Gilboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
